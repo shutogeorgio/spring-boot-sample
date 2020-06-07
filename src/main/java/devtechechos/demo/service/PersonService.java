96602a9a39cbba22edc2b8e0541959985012c4a5
shuGeorgio <dev.shudiscrete@gmail.com>
2020-06-07 21:02:50 +0900

    initial commit

---
+++ src/main/java/devtechechos/demo/service/PersonService.java
@@ -0,0 +1,35 @@
+package devtechechos.demo.service;
+
+import devtechechos.demo.dao.PersonDao;
+import devtechechos.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople() {
+        return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return personDao.selectPersonById(id);
+    }
+}
+
+
